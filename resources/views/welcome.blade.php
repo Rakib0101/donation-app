dfd941f01e56a04b5ddd71e0eb102c62fb88508e
Rakib0101 <rakib.01jan@gmail.com>
2024-03-03 21:23:33 +0600

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,22 @@
 
     <!-- Styles -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        .focus-color {
+            background-color: #24223e;
+            background-image: url('{{ asset('build/assets/text-bg.svg') }}');
+            background-position: center center;
+            -webkit-background-clip: text;
+            background-clip: text;
+            background-size: cover;
+            background-repeat: no-repeat;
+            color: #24223e;
+            font-weight: 900;
+            font-size: 20px;
+            line-height: 28px;
+            -webkit-text-fill-color: transparent;
+        }
+    </style>
 
 </head>
 
@@ -21,12 +37,30 @@
         <h2 class="text-2xl">Marketor</h2>
     </header>
     <main class="flex-grow">
-        <div class="w-full flex justify-center items-center py-24">
-            <div class="w-full max-w-xl mx-4 p-8 border border-gray-400 bg-primary-200 rounded-xl">
+        <div class="container mx-auto w-full flex flex-col justify-center items-center py-24">
+            <div class="focus-color my-8 p-5">
+                ক্যাশ আউট সর্বদা রাউন্ড ফিগার করবেন লাস্ট এ যেন জিরো(0) / পাঁচ (5) থাকে,
+                <br />
+                অনুগ্রহ করে কোন প্রকার পয়সা পাঠাবেন না,
+                <br />
+                অন্যথায় আপনার অর্ডার বাতিল করা হবে
+                <br />
+                <br/>
+                উদাহরণস্বরূপ : আপনার ক্যাশ আউট এমাউন্ট যদি 1143.83 হয় <br/> আপনি 1140 টাকা ক্যাশ আউট করবেন অথবা আপনার এমন
+                যদি হয় <br/> 1148.56 আপনি ক্যাশ আউট করবেন 1150 Tk অবশ্যই মনে রাখবেন <br/> শেষের সংখ্যা যেন জিরো(0) /পাঁচ (5)
+                হয়
+                <br />
+                <br/>
+                Best Regards
+                <br />
+                From Astropay
+            </div>
+            <div class="w-full !mx-5 max-w-2xl p-5 border border-gray-400 bg-primary-200 rounded-xl">
                 <form action="{{ route('payment') }}" class="flex flex-col gap-4" method="POST">
                     @csrf
-                    <label for="amount" class="text-xl">Donate Any Amount</label>
-                    <input type="number" name="amount" id="amount" class="py-2 px-5 rounded border border-gray-400" placeholder="donation amount">
+                    <label for="amount" class="text-xl">Please Insert Pay Amount</label>
+                    <input type="number" name="amount" id="amount" class="py-2 px-5 rounded border border-gray-400"
+                        placeholder="enter amount">
                     <button type="submit" class="bg-gray-900 text-white rounded py-2">Pay</button>
                 </form>
             </div>
